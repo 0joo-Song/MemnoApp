28128aa3f38461f9db9ae7cbdbadc180eb817b8e
0joo-Song <syoung7024@gmail.com>
2022-06-02 14:21:49 +0900

    vue - frontend

---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer>
+        &copy;2022 Memo App. All rights reseved.
+    </footer>
+</template>
+
+<style scoped>
+    footer {
+        padding: 25px;
+        border-top: 1px solid #eee;
+        background-color: #f7f7f7;
+    }
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+       0joo's Memo : )
+    </header>
+</template>
+ 
+<style scoped>
+    header {
+        text-align: center;
+        padding: 25px;
+        border-bottom: 1px solid #eee;
+        background-color: #f7f7f7;
+    }
+</style>
